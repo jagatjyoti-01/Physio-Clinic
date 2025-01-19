9337b1e4e12e5e6a04248a57b01e0e27234aedea
jagatjyoti-01 <jagatjyotidash33@gmail.com>
2025-01-19 20:06:07 +0530

    by me

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import PhysicalTherapySection from '../components/PhysicalTherapySection'
 
 function Home() {
   return (
     <div>
       <Header></Header>
       <Hero></Hero>
+      <PhysicalTherapySection/>
     </div>
   )
 }
